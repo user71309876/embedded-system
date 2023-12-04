9e8c4c0ba81186a1c3c11a7c1bb7c513a6250de3
user71309876 <93814905+user71309876@users.noreply.github.com>
2023-12-04 23:23:35 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express"
+import mysql from "mysql"
+
+const app = express()
+const port = 3020
+
+const db = mysql.createConnection({
+	host: 'svc.sel4.cloudtype.app',
+    port: 30640,
+	user: 'root',
+	password: 'mysql1234',
+	database: 'sensor',
+})
+
+db.connect()
+
+app.get('/', (req, res) => {
+    res.json({result: "스앱"})
+})
+
+
+app.get('/temperature', (req, res) => {
+    const sql = 'select * from temperature'
+    
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.json({result: "error"})
+            return console.log(err)
+        }
+        res.json(rows)
+    })
+})
+
+app.listen(port, () => {
+    console.log(`서버 실행됨 (port ${port})`)
+})
