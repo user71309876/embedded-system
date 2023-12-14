e04ceb15e0ce350a608936354cdb3fc3a8b1cbab
user71309876 <93814905+user71309876@users.noreply.github.com>
2023-12-14 18:20:57 +0900

    etc

--- server.js
+++ server.js
@@ -20,18 +20,20 @@ app.get("/", (req, res) => {
   res.json({ result: "스앱" });
 });
 
-var result = 0;
+var result = false;
+
 app.get("/warning", (req, res) => {
-  if (result == 0) {
-    result = 1;
+  if (result == false) {
+    result = true;
     res.json({ warning: "Get Ok" });
   }
 });
 
-app.get("/warning", (req, res) => {
-  if (result == 0) {
-    result = 1;
-    res.json({ warning: "Get Ok" });
+app.get("/buzzer", (req, res) => {
+  if (result) {
+    res.json({ warning: "noise" });
+  } else {
+    res.json({ warning: "quiet" });
   }
 });
 
